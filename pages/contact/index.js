a778ffeb45720a6ebf5a72bd939b615ebba97211
toxa16 <toxa16@gmail.com>
2019-10-29 21:34:51 +0200

    contact xs

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -12,31 +12,32 @@ export default function Contact() {
           <img className="Contact-Image" src="/contact.jpg" />
         </div>
 
-        <div className="container mt-5">
-          <div className="row">
-            <div className="col-12">
+        <div className="container">
+          <div>
+            <div className="Contact-TitleBlock">
               <div className="Contact-Title">We are here for you</div>
               <div className="Contact-Subtitle">
                 Drop us a line, call us, or even visit us.
               </div>
             </div>
 
-            <div className="col-6 mb-5">
-              <div className="Contact-Heading">Mail</div>
-              <div className="Contact-Paragraph">hello@schmiede.one</div>
-              <div className="Contact-Heading">Phone</div>
-              <div className="Contact-Paragraph">+49 (0) 211 97 63 49 26</div>
+            <div className="Contact-DataBlocks">
+              <div className="Contact-DataBlock">
+                <div className="Contact-Heading">Mail</div>
+                <div className="Contact-Paragraph">hello@schmiede.one</div>
+                <div className="Contact-Heading">Phone</div>
+                <div className="Contact-Paragraph">+49 (0) 211 97 63 49 26</div>
+              </div>
+              <div className="Contact-DataBlock">
+                <div className="Contact-Heading">Office</div>
+                <div className="Contact-Paragraph">Schmiede.ONE GmbH &amp; Co. KG</div>
+                <div className="Contact-Paragraph">Rather Straße 25</div>
+                <div className="Contact-Paragraph">40476, Düsseldorf </div>
+                <div className="Contact-Paragraph">Germany</div>
+              </div>
             </div>
 
-            <div className="col-6">
-              <div className="Contact-Heading">Office</div>
-              <div className="Contact-Paragraph">Schmiede.ONE GmbH &amp; Co. KG</div>
-              <div className="Contact-Paragraph">Rather Straße 25</div>
-              <div className="Contact-Paragraph">40476, Düsseldorf </div>
-              <div className="Contact-Paragraph">Germany</div>
-            </div>
-
-            <div className="col-12">
+            <div className="Contact-SocialBlock">
               <a className="mr-4" href="#">
                 <img
                   className="Contact-SocialImage"
