941ad43bf43bb7161b8397c993bfdbc887883911
toxa16 <toxa16@gmail.com>
2019-10-29 20:52:34 +0200

    init contact responsive

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -11,24 +11,53 @@ export default function Contact() {
         <div className="container">
           <img className="Contact-Image" src="/contact.jpg" />
         </div>
+
         <div className="container mt-5">
           <div className="row">
+            <div className="col-12">
+              <div className="Contact-Title">We are here for you</div>
+              <div className="Contact-Subtitle">
+                Drop us a line, call us, or even visit us.
+              </div>
+            </div>
+
+            <div className="col-6 mb-5">
+              <div className="Contact-Heading">Mail</div>
+              <div className="Contact-Paragraph">hello@schmiede.one</div>
+              <div className="Contact-Heading">Phone</div>
+              <div className="Contact-Paragraph">+49 (0) 211 97 63 49 26</div>
+            </div>
+
             <div className="col-6">
-              <div>We are here for you</div>
-              <div>Drop us a line, call us, or even visit us.</div>
+              <div className="Contact-Heading">Office</div>
+              <div className="Contact-Paragraph">Schmiede.ONE GmbH &amp; Co. KG</div>
+              <div className="Contact-Paragraph">Rather Straße 25</div>
+              <div className="Contact-Paragraph">40476, Düsseldorf </div>
+              <div className="Contact-Paragraph">Germany</div>
             </div>
-            <div className="col-3">
-              <h2>Mail</h2>
-              <p>hello@schmiede.one</p>
-              <h2>Phone</h2>
-              <p>+49 (0) 211 97 63 49 26</p>
-            </div>
-            <div className="col-3">
-              <h2>Office</h2>
-              <p>Schmiede.ONE GmbH &amp; Co. KG </p>
-              <p>Rather Straße 25</p>
-              <p>40476, Düsseldorf </p>
-              <p>Germany</p>
+
+            <div className="col-12">
+              <a className="mr-4" href="#">
+                <img
+                  className="Contact-SocialImage"
+                  src="/instagram.svg"
+                  alt="Instagram"
+                />
+              </a>
+              <a className="mr-4" href="#">
+                <img
+                  className="Contact-SocialImage"
+                  src="/facebook.svg"
+                  alt="Facebook"
+                />
+              </a>
+              <a href="#">
+                <img
+                  className="Contact-SocialImage"
+                  src="/linkedin.svg"
+                  alt="LinkedIn"
+                />
+              </a>
             </div>
           </div>
         </div>
