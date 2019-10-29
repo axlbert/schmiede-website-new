a8cb1632a07f18fbd1f0e82db70a4e2968475b05
toxa16 <toxa16@gmail.com>
2019-10-29 18:17:27 +0200

    init about page header

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
 import './about.css';
-import Nav from '../../components/nav'
+import Nav from '../../components/nav';
+
+const headerStyle = {
+  backgroundImage: 'url("/about.jpg")',
+};
 
 export default function About() {
   return (
     <div className="About">
       <Nav />
-      <header>
-        <img className="About-HeaderImage" src="/about.jpg" />
+      <header className="About-Header" style={headerStyle}>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <div>
+                <span className="About-HeaderTitle">
+                  We are a gang of creatives, developers, engeneers and managers 
+                  from around the world working in Düsseldorf, Germany. 
+                </span>
+              </div>
+              <div className="About-HeaderSubtitle">
+                We are an innovation lab from GRIMME group. We give our 
+                best everyday to make a difference in the AgTech world.
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
