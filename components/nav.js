aaae9d7cc3874933cd0b477e3b63013714df20e2
toxa16 <toxa16@gmail.com>
2019-10-29 14:03:42 +0200

    install css parser

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 
+import './nav.css';
+
 const links = [
   { href: 'https://zeit.co/now', label: 'ZEIT' },
   { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
@@ -25,30 +27,7 @@ const Nav = () => (
     </ul>
 
     <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: #067df7;
-        text-decoration: none;
-        font-size: 13px;
-      }
+      
     `}</style>
   </nav>
 )
