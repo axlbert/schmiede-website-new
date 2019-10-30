5dc1120fe1cac2429191034f3757312de9cf4606
toxa16 <toxa16@gmail.com>
2019-10-30 11:59:23 +0200

    init work markup

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
+import './work.css';
 import Nav from '../../components/nav';
 
 export default function Work() {
   return (
     <div>
       <Nav />
-      <div>Work</div>
+      <div className="container">
+        <header>
+          <img className="Work-HeaderImage" src="/work/work.jpg" />
+        </header>
+
+        <div style={{ display: 'flex' }}>
+          <div>All</div>
+          <div>Robotics</div>
+          <div>Development</div>
+          <div>Farming</div>
+        </div>
+
+        
+      </div>
     </div>
   );
 }
