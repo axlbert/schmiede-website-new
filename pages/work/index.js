24921c02edd9563ae5e3a21ce7e0149efab0189b
toxa16 <toxa16@gmail.com>
2019-10-30 12:16:05 +0200

    init project grid

--- pages/work/index.js
+++ pages/work/index.js
@@ -19,7 +19,45 @@ export default function Work() {
           <div>Farming</div>
         </div>
 
-        
+        <div className="ProjectGrid">
+          <div>
+            <img className="ProjectGrid-ItemImage" src="/work/project-1.jpg" />
+            <div className="ProjectGrid-ItemTitle">Harvey.one</div>
+            <div className="ProjectGrid-ItemSubtitle">Robotics</div>
+          </div>
+          <div>
+            <img className="ProjectGrid-ItemImage" src="/work/project-2.jpg" />
+            <div className="ProjectGrid-ItemTitle">CNHI Konfigurator</div>
+            <div className="ProjectGrid-ItemSubtitle">Development</div>
+          </div>
+          <div>
+            <img className="ProjectGrid-ItemImage" src="/work/project-3.jpg" />
+            <div className="ProjectGrid-ItemTitle">Indoor Farming</div>
+            <div className="ProjectGrid-ItemSubtitle">Farming</div>
+          </div>
+          
+          <div>
+            <img className="ProjectGrid-ItemImage" src="/work/project-1.jpg" />
+            <div className="ProjectGrid-ItemTitle">Harvey.one</div>
+            <div className="ProjectGrid-ItemSubtitle">Robotics</div>
+          </div>
+          <div>
+            <img className="ProjectGrid-ItemImage" src="/work/project-2.jpg" />
+            <div className="ProjectGrid-ItemTitle">CNHI Konfigurator</div>
+            <div className="ProjectGrid-ItemSubtitle">Development</div>
+          </div>
+          <div>
+            <img className="ProjectGrid-ItemImage" src="/work/project-3.jpg" />
+            <div className="ProjectGrid-ItemTitle">Indoor Farming</div>
+            <div className="ProjectGrid-ItemSubtitle">Farming</div>
+          </div>
+        </div>
+
+
+        <br />
+        <br />
+
+
       </div>
     </div>
   );
