cb23d36277c528e0df729899f921c58a9815df98
toxa16 <toxa16@gmail.com>
2019-11-02 01:36:10 +0200

    init member card personal overlay

--- components/member-card/index.js
+++ components/member-card/index.js
@@ -3,26 +3,75 @@ import React from 'react';
 import './member-card.css';
 import DotControls from '../dot-controls';
 
+function ProfessionalOverlay() {
+  return (
+    <div className="MemberCard-Professional Card-ImageOverlay Card-ImageOverlay_gray">
+      <header>
+        <div className="MemberCard-HeaderTextSmall">Team member since</div>
+        <div className="MemberCard-HeaderText">2017</div>
+      </header>
+      <div className="MemberCard-Quote">
+        “Make it simple but significant.”
+      </div>
+      <div className="MemberCard-Description">
+        Coming from the fashion industry, I am working at Schmiede.one 
+        on things that display my true passions which are: food, 
+        agriculture, and innovative business models.
+        My project range is, from our test field to pure software projects 
+        to Indoor farming, which makes working at Schmiede.one very diverse.
+      </div>
+    </div>
+  );
+}
+
 export default function MemberCard() {
   return (
     <div className="Card">
       <div className="Card-ImageWrapper">
         <img className="Card-Image" src="/about/member-2.jpg" />
-        <div className="MemberCard-Professional Card-ImageOverlay Card-ImageOverlay_gray">
+        <div className="MemberCard-Professional Card-ImageOverlay Card-ImageOverlay_red">
           <header>
-            <div className="MemberCard-HeaderTextSmall">Team member since</div>
-            <div className="MemberCard-HeaderText">2017</div>
+            <div className="MemberCard-HeaderText">Lima, Peru</div>
           </header>
           <div className="MemberCard-Quote">
-            “Make it simple but significant.”
+            “Give me a book and chocolates, i'll be in paradise.”
           </div>
-          <div className="MemberCard-Description">
-            Coming from the fashion industry, I am working at Schmiede.one 
-            on things that display my true passions which are: food, 
-            agriculture, and innovative business models.
-            My project range is, from our test field to pure software projects 
-            to Indoor farming, which makes working at Schmiede.one very diverse.
-          </div>
+          <table className="MemberCard-Table">
+            <tbody>
+              <tr className="MemberCard-TableTr">
+                <th className="MemberCard-TableTh MemberCard_borderBottom">
+                  Book:
+                </th>
+                <td className="MemberCard-TableTd MemberCard_borderBottom">
+                  El Extranjero
+                </td>
+              </tr>
+              <tr className="MemberCard-TableTr">
+                <th className="MemberCard-TableTh MemberCard_borderBottom">
+                  Color:
+                </th>
+                <td className="MemberCard-TableTd MemberCard_borderBottom">
+                  Black
+                </td>
+              </tr>
+              <tr className="MemberCard-TableTr">
+                <th className="MemberCard-TableTh MemberCard_borderBottom">
+                  Food:
+                </th>
+                <td className="MemberCard-TableTd MemberCard_borderBottom">
+                  Ceviche
+                </td>
+              </tr>
+              <tr className="MemberCard-TableTr">
+                <th className="MemberCard-TableTh">
+                  Vacation Spot:
+                </th>
+                <td className="MemberCard-TableTd">
+                  Italy
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
       <div
