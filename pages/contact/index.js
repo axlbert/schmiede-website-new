a0829af7596e253a78ce612bcc7bf73584d201c4
toxa16 <toxa16@gmail.com>
2019-11-01 02:07:49 +0200

    init contact md markup

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -36,36 +36,53 @@ export default function Contact() {
               Drop us a line, call us, or even visit us.
             </span>
           </section>
-          
 
           <section className="Contact-InfoSection">
             <div className="Contact-InfoBlock">
               <span className="Contact-Heading">Mail</span>
               <span className="Contact-Paragraph">
-                <a className="Contact-MailLink" href="#">hello@schmiede.one</a>
+                <a className="Contact-MailLink" href="#">
+                  hello@schmiede.one
+                </a>
               </span>
               <span className="Contact-Heading">Phone</span>
-              <span className="Contact-Paragraph">+49 (0) 211 97 63 49 26</span>
+              <span className="Contact-Paragraph">
+                +49 (0) 211 97 63 49 26
+              </span>
             </div>
 
             <div className="Contact-InfoBlock">
               <span className="Contact-Heading">Office</span>
-              <span className="Contact-Paragraph">Schmiede.ONE GmbH &amp; Co. KG</span>
-              <span className="Contact-Paragraph">Rather Straße 25 </span>
-              <span className="Contact-Paragraph">40476, Düsseldorf </span>
+              <span className="Contact-Paragraph">
+                Schmiede.ONE GmbH &amp; Co. KG
+              </span>
+              <span className="Contact-Paragraph">Rather Straße 25</span>
+              <span className="Contact-Paragraph">40476, Düsseldorf</span>
               <span className="Contact-Paragraph">Germany</span>
             </div>
           </section>
 
           <section className="Contact-SocialSection">
             <a className="Contact-SocialLink" href="#" title="Instagram">
-              <img src="/contact/instagram.svg" alt="Instagram" />
+              <img
+              className="Contact-SocialImage"
+                src="/contact/instagram.svg"
+                alt="Instagram"
+              />
             </a>
             <a className="Contact-SocialLink" href="#" title="Facebook">
-              <img src="/contact/facebook.svg" alt="Facebook" />
+              <img
+                className="Contact-SocialImage"
+                src="/contact/facebook.svg"
+                alt="Facebook"
+              />
             </a>
             <a className="Contact-SocialLink" href="#" title="LinkedIn">
-              <img src="/contact/linkedin.svg" alt="LinkedIn" />
+              <img
+              className="Contact-SocialImage"
+                src="/contact/linkedin.svg"
+                alt="LinkedIn"
+              />
             </a>
           </section>
         </main>
