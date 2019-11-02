72ae7aabac202192114a779e7d825a83b9395ba6
toxa16 <toxa16@gmail.com>
2019-11-02 14:05:01 +0200

    post carousel image aspect ratio

--- components/post-carousel/index.js
+++ components/post-carousel/index.js
@@ -6,7 +6,13 @@ import DotControls from '../dot-controls';
 export default function PostCarousel() {
   return (
     <div className="PostCarousel">
-      <img className="PostCarousel-Image" src="/work/carousel-1.jpg" />
+      <div className="PostCarousel-ImageWrapper">
+        <img
+          className="PostCarousel-Image"
+          src="/work/carousel-1.jpg"
+          alt="Carousel Image 1"
+        />
+      </div>
       <div className="PostCarousel-Controls">
         <DotControls length={3} activeIndex={0} />
       </div>
