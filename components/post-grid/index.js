8edabc7a2f74b235a3de78684d0c0f8be40de1d0
toxa16 <toxa16@gmail.com>
2019-11-05 00:33:45 +0200

    init post grid loading state

--- components/post-grid/index.js
+++ components/post-grid/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PostCard from '../post-card';
 
 import posts from '../../data/posts';
 
-const renderPosts = () => {
+function renderPosts() {
   return posts.map((x, i) => {
     return (
       <PostCard
@@ -17,9 +17,21 @@ const renderPosts = () => {
 }
 
 export default function PostGrid() {
+  const [isLoading, setLoading] = useState(true);
+
+  const renderBody = () => {
+    if (isLoading) {
+      return <div>Loading...</div>;
+    } else {
+      return (<div className="Grid">
+        { renderPosts() }
+      </div>);
+    }
+  };
+
   return (
-    <div className="Grid">
-      { renderPosts() }
+    <div>
+      { renderBody() }
     </div>
   );
 }
