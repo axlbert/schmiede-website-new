b424eac8eb7aaf7bd9384c60d20ef05aae98b523
toxa16 <toxa16@gmail.com>
2019-11-05 01:04:08 +0200

    init fetching posts from backend

--- components/post-grid/index.js
+++ components/post-grid/index.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import PostCard from '../post-card';
 
-import posts from '../../data/posts';
+import posts0 from '../../data/posts';
 
-function renderPosts() {
-  return posts.map((x, i) => {
+/*function renderPosts() {
+  return posts0.map((x, i) => {
     return (
       <PostCard
         key={i}
@@ -14,13 +14,52 @@ function renderPosts() {
       />
     );
   })
-}
+}*/
 
 export default function PostGrid() {
+  const [isError, setError] = useState(false);
   const [isLoading, setLoading] = useState(true);
+  const [posts, setPosts] = useState();
+
+  function initPosts(rawPosts) {
+    const convertedPosts = [];
+    rawPosts.map(x => {
+      const p = {
+        title: x.title.rendered,
+        subtitle: 'Subtitle',
+        imageSrc: '/work/project-2.jpg',
+      };
+      convertedPosts.push(p);
+    });
+    setPosts(convertedPosts);
+    setLoading(false);
+  }
+
+  function renderPosts() {
+    return posts.map((x, i) => {
+      return (
+        <PostCard
+          key={i}
+          onClick={ () => {} }
+          {...x}
+        />
+      );
+    })
+  }
+
+  useEffect(() => {
+    //const backendUrl = 'http://schmiede.one/index.php/wp-json/wp/v2/posts';
+    const backendUrl = 'http://localhost/wp-blog-api/index.php/wp-json/wp/v2/posts';
+    fetch(backendUrl)
+      .then(res => res.json())
+      //.then(data => data.json())
+      .then(_posts => initPosts(_posts));
+  }, []);
 
   const renderBody = () => {
-    if (isLoading) {
+    if (isError) {
+      return <div style={{ color: 'red' }}>Error occurred.</div>
+    } else if (isLoading) {
       return <div>Loading...</div>;
     } else {
       return (<div className="Grid">
