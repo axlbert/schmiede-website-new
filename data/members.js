00da34df9e0d645cee03d5efd98ff42a498ae945
Alex Langer <aclanger131@gmail.com>
2019-11-06 17:44:56 +0100

    replace placeholders

--- data/members.js
+++ data/members.js
@@ -2,13 +2,13 @@
  * TEAM MEMBERS DATA.
  */
 
-const ollie = {
-  name: 'Ollie',
-  role: 'Developer',
+const georg = {
+  name: 'Georg',
+  role: 'Mechanical',
   imageSrc: '/about/member-1.jpg',
   personal: {
-    location: 'Kyiv, Ukraine',
-    quote: `Give me wine and cards, i'll be in purgatory.`,
+    location: 'Düsseldorf',
+    quote: `I like tractors`,
     favorites: new Map([
       ['Book', 'Harry Potter'],
       ['Color', 'Black'],
@@ -29,13 +29,13 @@ const ollie = {
   },
 };
 
-const analisa = {
-  name: 'Analisa',
-  role: 'Designer',
+const anna = {
+  name: 'Anna',
+  role: 'Projects',
   imageSrc: '/about/member-2.jpg',
   personal: {
-    location: 'Lima, Peru',
-    quote: `Give me a book and chocolates, i'll be in paradise.`,
+    location: 'Düsseldorf',
+    quote: `I like sustainable thinks`,
     favorites: new Map([
       ['Book', 'El Extranjero'],
       ['Color', 'Black'],
@@ -45,7 +45,7 @@ const analisa = {
   },
   professional: {
     sinceYear: 2018,
-    quote: `Make it simple but significant.`,
+    quote: `lets grow food`,
     description: `
       2: Coming from the fashion industry, I am working at Schmiede.one 
       on things that display my true passions which are: food, 
@@ -56,13 +56,13 @@ const analisa = {
   },
 };
 
-const leonarda = {
-  name: 'Leonarda',
-  role: 'Project Manager',
+const rahul = {
+  name: 'Rahul',
+  role: 'Robotics',
   imageSrc: '/about/member-3.jpg',
   personal: {
-    location: 'Düsseldorf, Germany',
-    quote: `Give me a book and chocolates, i'll be in paradise.`,
+    location: 'Düsseldorf',
+    quote: `Robots are awesome`,
     favorites: new Map([
       ['Book', 'El Extranjero'],
       ['Color', 'Black'],
@@ -72,7 +72,7 @@ const leonarda = {
   },
   professional: {
     sinceYear: 2017,
-    quote: `Divide and rule.`,
+    quote: `Robots are awesome`,
     description: `
       3: Coming from the fashion industry, I am working at Schmiede.one 
       on things that display my true passions which are: food, 
@@ -83,7 +83,167 @@ const leonarda = {
   },
 };
 
-//const members = [ollie];
-//const members = [leonarda];
-const members = [ollie, analisa, leonarda];
+const andy = {
+  name: 'Andy',
+  role: 'Coding',
+  imageSrc: '/about/member-8.jpg',
+  personal: {
+    location: 'Düsseldorf',
+    quote: `I like code`,
+    favorites: new Map([
+      ['Book', 'El Extranjero'],
+      ['Color', 'Black'],
+      ['Food', 'Ceviche'],
+      ['Vacation Spot', 'Italy'],
+    ]),
+  },
+  professional: {
+    sinceYear: 2018,
+    quote: `lets code`,
+    description: `
+      2: Coming from the fashion industry, I am working at Schmiede.one 
+      on things that display my true passions which are: food, 
+      agriculture, and innovative business models.
+      My project range is, from our test field to pure software projects 
+      to Indoor farming, which makes working at Schmiede.one very diverse.
+    `,
+  },
+};
+
+const alex = {
+  name: 'Alex',
+  role: 'Operations',
+  imageSrc: '/about/member-4.jpg',
+  personal: {
+    location: 'Düsseldorf',
+    quote: `We re moving heavy metal`,
+    favorites: new Map([
+      ['Book', 'El Extranjero'],
+      ['Color', 'Black'],
+      ['Food', 'Ceviche'],
+      ['Vacation Spot', 'Italy'],
+    ]),
+  },
+  professional: {
+    sinceYear: 2017,
+    quote: `Lets solve some problems`,
+    description: `
+      2: Coming from the fashion industry, I am working at Schmiede.one 
+      on things that display my true passions which are: food, 
+      agriculture, and innovative business models.
+      My project range is, from our test field to pure software projects 
+      to Indoor farming, which makes working at Schmiede.one very diverse.
+    `,
+  },
+};
+
+const jakub = {
+  name: 'Jakub',
+  role: 'Frontend',
+  imageSrc: '/about/member-5.jpg',
+  personal: {
+    location: 'Düsseldorf',
+    quote: `I like javascript`,
+    favorites: new Map([
+      ['Book', 'El Extranjero'],
+      ['Color', 'Black'],
+      ['Food', 'Ceviche'],
+      ['Vacation Spot', 'Italy'],
+    ]),
+  },
+  professional: {
+    sinceYear: 2017,
+    quote: `lets code`,
+    description: `
+      2: Coming from the fashion industry, I am working at Schmiede.one 
+      on things that display my true passions which are: food, 
+      agriculture, and innovative business models.
+      My project range is, from our test field to pure software projects 
+      to Indoor farming, which makes working at Schmiede.one very diverse.
+    `,
+  },
+};
+
+const omri = {
+  name: 'Omri',
+  role: 'Data',
+  imageSrc: '/about/member-6.jpg',
+  personal: {
+    location: 'Bonn',
+    quote: `I like statistics`,
+    favorites: new Map([
+      ['Book', 'El Extranjero'],
+      ['Color', 'Black'],
+      ['Food', 'Ceviche'],
+      ['Vacation Spot', 'Italy'],
+    ]),
+  },
+  professional: {
+    sinceYear: 2018,
+    quote: `lets do something great`,
+    description: `
+      2: Coming from the fashion industry, I am working at Schmiede.one 
+      on things that display my true passions which are: food, 
+      agriculture, and innovative business models.
+      My project range is, from our test field to pure software projects 
+      to Indoor farming, which makes working at Schmiede.one very diverse.
+    `,
+  },
+};
+
+const niklas = {
+  name: 'Niklas',
+  role: 'Projects',
+  imageSrc: '/about/member-9.jpg',
+  personal: {
+    location: 'Düsseldorf',
+    quote: `I like projects`,
+    favorites: new Map([
+      ['Book', 'El Extranjero'],
+      ['Color', 'Black'],
+      ['Food', 'Ceviche'],
+      ['Vacation Spot', 'Italy'],
+    ]),
+  },
+  professional: {
+    sinceYear: 2018,
+    quote: `lets do something great`,
+    description: `
+      2: Coming from the fashion industry, I am working at Schmiede.one 
+      on things that display my true passions which are: food, 
+      agriculture, and innovative business models.
+      My project range is, from our test field to pure software projects 
+      to Indoor farming, which makes working at Schmiede.one very diverse.
+    `,
+  },
+};
+
+const pavi = {
+  name: 'Pavi',
+  role: 'Projects',
+  imageSrc: '/about/member-10.jpg',
+  personal: {
+    location: 'Düsseldorf',
+    quote: `I like code`,
+    favorites: new Map([
+      ['Book', 'El Extranjero'],
+      ['Color', 'Black'],
+      ['Food', 'Ceviche'],
+      ['Vacation Spot', 'Italy'],
+    ]),
+  },
+  professional: {
+    sinceYear: 2018,
+    quote: `lets debug`,
+    description: `
+      2: Coming from the fashion industry, I am working at Schmiede.one 
+      on things that display my true passions which are: food, 
+      agriculture, and innovative business models.
+      My project range is, from our test field to pure software projects 
+      to Indoor farming, which makes working at Schmiede.one very diverse.
+    `,
+  },
+};
+
+const members = [georg, anna, rahul,andy,alex, jakub, omri, pavi, niklas];
 export default members;
