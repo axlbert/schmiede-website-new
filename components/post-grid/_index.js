56886b2aef3fb956df07992d9e07d72c16506fff
toxa16 <toxa16@gmail.com>
2019-11-06 15:27:56 +0200

    moving new post grid component to separate file

---
+++ components/post-grid/_index.js
@@ -0,0 +1,184 @@
+import React, { useState, useEffect } from 'react';
+
+import PostCard from '../post-card';
+import PostDialog from '../post-dialog';
+import Post from '../post';
+import PostFilters from '../post-filters';
+import PostTag from '../post-tag.enum';
+
+const baseBackendUrl = 'http://schmiede.one/index.php/wp-json';
+//const baseBackendUrl = 'http://localhost/wp-blog-api/index.php/wp-json';
+
+function fetchPostTag(rawPost) {
+  const tagId = rawPost.tags[0];
+  const url = `${baseBackendUrl}/wp/v2/tags/${tagId}`;
+  if (!tagId) return;
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        resolve(data.name);
+      })
+      .catch(reject);
+  });
+}
+
+function fetchPostMedia(rawPost) {
+  const mediaId = rawPost.featured_media;
+  const url = `${baseBackendUrl}/wp/v2/media/${mediaId}`;
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const imageSrc = data.guid ? data.guid.rendered : null;
+        resolve(imageSrc);
+      })
+      .catch(reject);
+  });
+}
+
+function fetchPostData(rawPost) {
+  const fetchers = [
+    fetchPostMedia(rawPost),
+    fetchPostTag(rawPost),
+  ];
+  return Promise.all(fetchers)
+    .then(([mediaSrc, tag]) => {
+      return {
+        title: rawPost.title.rendered,
+        subtitle: tag,
+        imageSrc: mediaSrc,
+        content: rawPost.content.rendered,
+      };
+    });
+}
+
+/**
+ * Post grid component.
+ */
+export default function PostGrid() {
+  const [isError, setError] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [posts, setPosts] = useState();
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [previousPost, setPreviousPost] = useState(null);
+  const [nextPost, setNextPost] = useState(null);
+  const [filter, setFilter] = useState(PostTag.ALL);
+
+  function initPosts(rawPosts) {
+    const postPromises = rawPosts.map(x => {
+      return fetchPostData(x);
+    });
+    return Promise.all(postPromises)
+      .then(fetchedPosts => {
+        setPosts(fetchedPosts);
+        setLoading(false);
+      });
+  }
+
+  function renderPosts() {
+    return posts.map((x, i) => {
+      if (filter === PostTag.ALL || x.subtitle === filter) {
+        return (
+          <PostCard
+            key={i}
+            onClick={ e => handleCardClick(e, x, i) }
+            {...x}
+          />
+        );
+      }
+    })
+  }
+
+  function handleCardClick(e, x, _i) {
+    const filteredPosts = posts.filter(x => {
+      return filter === PostTag.ALL || x.subtitle === filter;
+    });
+    const i = filteredPosts.indexOf(x);
+    setSelectedPost(x);
+    if (i > 0) {
+      setPreviousPost(filteredPosts[i - 1]);
+    }
+    if (i < filteredPosts.length - 1) {
+      setNextPost(filteredPosts[i + 1]);
+    }
+    setDialogOpen(true);
+  }
+
+  useEffect(() => {
+    const backendUrl = `${baseBackendUrl}/wp/v2/posts`;
+    // for cleaning up the effect
+    const abortController = new AbortController();
+
+    fetch(backendUrl, { signal: abortController.signal})
+      .then(res => res.json())
+      .then(_posts => initPosts(_posts))
+      .catch(err => {
+        setError(true);
+        console.error(err);
+      });
+    
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  function renderBody() {
+    if (isError) {
+      return <div style={{ color: 'red' }}>Error occurred.</div>
+    } else if (isLoading) {
+      return <div>Loading...</div>;
+    } else {
+      return (<div className="Grid">
+        { renderPosts() }
+      </div>);
+    }
+  };
+
+  function handleDialogClose() {
+    setDialogOpen(false);
+    setSelectedPost(null);
+    setPreviousPost(null);
+    setNextPost(null);
+  }
+
+  function shiftPost(currentPost) {
+    const filteredPosts = posts.filter(x => {
+      return filter === PostTag.ALL || x.subtitle === filter;
+    });
+    const i = filteredPosts.indexOf(currentPost);
+    setSelectedPost(filteredPosts[i]);
+    if (i > 0) {
+      setPreviousPost(filteredPosts[i - 1]);
+    } else {
+      setPreviousPost(null);
+    }
+    if (i < filteredPosts.length - 1) {
+      setNextPost(filteredPosts[i + 1]);
+    } else {
+      setNextPost(null);
+    }
+    const muiDialogContainer = document.querySelector('.MuiDialog-container');
+    muiDialogContainer.scroll(0, 0);
+  }
+
+  return (
+    <div>
+      <PostFilters filter={filter} onChange={setFilter} />
+      { renderBody() }
+      <PostDialog open={isDialogOpen} onClose={handleDialogClose}>
+        {
+          selectedPost &&
+            <Post
+              {...selectedPost}
+              previous={previousPost}
+              next={nextPost}
+              onPreviousClick={ () => shiftPost(previousPost) }
+              onNextClick={ () => shiftPost(nextPost) }
+            />
+        }
+      </PostDialog>
+    </div>
+  );
+}
--- components/post-section/index.js
+++ components/post-section/index.js
@@ -1,105 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
-import posts0 from '../../data/posts';
-import PostCard from '../post-card';
-import PostDialog from '../post-dialog';
-import Post from '../post';
 import PostTag from '../post-tag.enum';
+import PostGrid from '../post-grid';
 
-/**
- * Post grid & dialog presentational component.
- * @param {*} param0 
- */
-function PostGrid({ posts, filter }) {
-  const [isDialogOpen, setDialogOpen] = useState(false);
-  const [filteredPosts, setFilteredPosts] = useState(posts);
-  const [currentPost, setCurrentPost] = useState(null);
-  const [previousPost, setPreviousPost] = useState(null);
-  const [nextPost, setNextPost] = useState(null);
+import posts0 from '../../data/posts';
 
-  /* filtering posts */
-  useEffect(() => {
-    const filtered = posts.filter(x => {
-      return !filter ||
-        filter === PostTag.ALL ||
-        x.subtitle === filter;
-    });
-    setFilteredPosts(filtered);
-  }, [posts, filter]);
-  
-  /* on post card click */
-  function handleCardClick(e, x, i) {
-    setCurrentPost(x);
-    if (i > 0) {
-      setPreviousPost(filteredPosts[i - 1]);
-    }
-    if (i < filteredPosts.length - 1) {
-      setNextPost(filteredPosts[i + 1]);
-    }
-    setDialogOpen(true);
-  }
-
-  /* on dialog close */
-  function handleDialogClose() {
-    setDialogOpen(false);
-    setCurrentPost(null);
-    setPreviousPost(null);
-    setNextPost(null);
-  }
-
-  /* change to next or previous post */
-  function shiftPost(x) {
-    const i = filteredPosts.indexOf(x);
-    setCurrentPost(filteredPosts[i]);
-    if (i > 0) {
-      setPreviousPost(filteredPosts[i - 1]);
-    } else {
-      setPreviousPost(null);
-    }
-    if (i < filteredPosts.length - 1) {
-      setNextPost(filteredPosts[i + 1]);
-    } else {
-      setNextPost(null);
-    }
-    const muiDialogContainer = document.querySelector('.MuiDialog-container');
-    muiDialogContainer.scroll(0, 0);
-  }
-
-  /* rendering filtered posts */
-  function renderPosts() {
-    return filteredPosts.map((x, i) => {
-      return <PostCard key={i} {...x} onClick={ e => handleCardClick(e, x, i) } />
-    });
-  }
-
-  return (
-    <div>
-      <div className="Grid">
-        { renderPosts() }
-      </div>
-      <PostDialog open={isDialogOpen} onClose={handleDialogClose}>
-        <Post 
-          {...currentPost}
-          previous={previousPost}
-          next={nextPost}
-          onPreviousClick={ () => shiftPost(previousPost) }
-          onNextClick={ () => shiftPost(nextPost) }
-        />
-      </PostDialog>
-    </div>
-  );
-}
+const baseBackendUrl = 'http://schmiede.one/index.php/wp-json';
+//const baseBackendUrl = 'http://localhost/wp-blog-api/index.php/wp-json';
 
 /**
  * Post section component.
  */
 export default function PostSection() {
+  const [isError, setError] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [posts, setPosts] = useState(posts0);
+  const [filter, setFilter] = useState(PostTag.ALL);
+
+  function renderGrid() {
+    if (isError) {
+      return <div style={{ color: 'red' }}>Error occurred.</div>
+    } else if (isLoading) {
+      return <div>Loading...</div>;
+    } else if (posts) {
+      return <PostGrid posts={posts} filter={filter} />;
+    }
+  };
+
   return (
     <section>
       <div>
         FILTERS
       </div>
-      <PostGrid posts={posts0} filter={PostTag.ALL} />
+      { renderGrid() }
     </section>
   );
 }
