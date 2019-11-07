56c9a6c43c3f7d3bff59ce295600fe3dbfda380a
Ainuddin Faizan <andyfaizan@gmail.com>
2019-11-07 17:21:20 +0100

    Update deploy script and add nginx

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,10 @@ VERSION=$(cat package.json \
 echo "Deploying version $VERSION"
 
 tag="schmiede-website"
-aws_tag="schmiede-website"
-tagged_image=436054152060.dkr.ecr.eu-central-1.amazonaws.com/${aws_tag}:${VERSION}
+proxy_tag="schmiede-website-proxy"
+
+tagged_image=436054152060.dkr.ecr.eu-central-1.amazonaws.com/${tag}:${VERSION}
+tagged_image_proxy=436054152060.dkr.ecr.eu-central-1.amazonaws.com/${proxy_tag}:latest
 
 login="$(aws ecr get-login --no-include-email --region eu-central-1)"
 $login
@@ -21,8 +23,12 @@ echo "Building docker..."
 docker build -f Dockerfile -t ${tag} .
 docker tag ${tag}:latest ${tagged_image}
 
+docker build -f Dockerfile-nginx -t ${proxy_tag} .
+docker tag ${proxy_tag}:latest ${tagged_image_proxy}
+
 echo "Pushing to AWS..."
 docker push ${tagged_image}
+docker push ${tagged_image_proxy}
 
 # cluster config needs to created beforehand
 # guide: https://github.com/aws/amazon-ecs-cli/blob/master/README.md#configuring-the-cli
@@ -33,4 +39,4 @@ echo "Stopping currently running service..."
 ecs-cli compose service --cluster-config ${CLUSTER_NAME} stop
 
 echo "Creating and starting service..."
-TAGGED_IMAGE=${tagged_image} ecs-cli compose service --cluster-config ${CLUSTER_NAME} up
+TAGGED_IMAGE=${tagged_image} TAGGED_IMAGE_PROXY=${tagged_image_proxy} ecs-cli compose service --cluster-config ${CLUSTER_NAME} up
