887ba383dbb8691a0faf1b5a54fd32efc8d0f791
Ainuddin Faizan <andyfaizan@gmail.com>
2019-11-07 13:15:12 +0100

    Correct cluster name

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ VERSION=$(cat package.json \
 echo "Deploying version $VERSION"
 
 image="app"
-tag="schmiede_website"
-aws_tag="schmiede_website"
+tag="schmiede-website"
+aws_tag="schmiede-website"
 tagged_image=436054152060.dkr.ecr.eu-central-1.amazonaws.com/${aws_tag}:${VERSION}
 
 login="$(aws ecr get-login --no-include-email --region eu-central-1)"
@@ -28,8 +28,10 @@ docker push ${tagged_image}
 # cluster config needs to created beforehand
 # guide: https://github.com/aws/amazon-ecs-cli/blob/master/README.md#configuring-the-cli
 
+CLUSTER_NAME=schmiede-website
+
 echo "Stopping currently running service..."
-ecs-cli compose service --cluster-config schmiede_website stop
+ecs-cli compose service --cluster-config ${CLUSTER_NAME} stop
 
 echo "Creating and starting service..."
-TAGGED_IMAGE=${tagged_image} ecs-cli compose service --cluster-config schmiede_website up
+TAGGED_IMAGE=${tagged_image} ecs-cli compose service --cluster-config ${CLUSTER_NAME} up
