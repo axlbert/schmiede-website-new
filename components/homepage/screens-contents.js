58e65cf9dc2051c5caf608deb5cbd10ea5fec155
axlbert <aclanger131@gmail.com>
2019-11-07 12:16:59 +0100

    add new images and text

--- components/homepage/screens-contents.js
+++ components/homepage/screens-contents.js
@@ -2,30 +2,30 @@ import HomepageScreen from './homepage-screen.enum';
 
 export const initialContents = {
   title: 'We move heavy metal',
-  subtitle: 'Hi there, would you like to know about:',
+  subtitle: 'What would you like to learn more about?',
   links: [
-    { label: 'us', href: '/about', screen: HomepageScreen.US },
-    { label: 'projects', href: '/work', screen: HomepageScreen.PROJECTS },
+    { label: 'The Company', href: '/about', screen: HomepageScreen.US },
+    { label: 'Our Work', href: '/work', screen: HomepageScreen.PROJECTS },
   ],
 };
 
 export const usContents = {
-  title: 'Want to go deeper?',
-  subtitle: 'Choose whatever you want to know about us:',
+  title: 'What aspect about schmiede.one are you most interested in?',
+  subtitle: 'Pick a category',
   links: [
-    { label: 'culture', href: '/about#culture' },
-    { label: 'team', href: '/about#team' },
-    { label: 'office', href: '/about#office' },
-    { label: 'join us', href: '/about#join-us' },
+    { label: 'Culture', href: '/about#culture' },
+    { label: 'Team', href: '/about#team' },
+    { label: 'Our Space', href: '/about#office' },
+    { label: 'Job Openings', href: '/about#join-us' },
   ],
 };
 
 export const projectsContents = {
-  title: 'Want to go deeper?',
-  subtitle: 'Choose between out projects\' subjects:',
+  title: 'Which domain of our work are you curious about?',
+  subtitle: 'Choose a subject',
   links: [
     { label: 'robotics', href: '/work#robotics' },
-    { label: 'development', href: '/work#development' },
+    { label: 'software development', href: '/work#development' },
     { label: 'farming', href: '/work#farming' },
   ],
 };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, Component, useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 
@@ -6,82 +6,114 @@ import './homepage.css';
 import Main from '../components/homepage/main';
 import HomepageScreen from '../components/homepage/homepage-screen.enum';
 
+import { initGA, logPageView } from '../utils/analytics';
+
 /**
  * Homepage component.
  */
-export default function Homepage() {
-  // height-100 fix for mobile browsers
-  const [windowInnerWidth, setInnerWidth] = useState(0);
-  function setRelativeVH() {
-    const newWidth = window.innerWidth;
-    if (newWidth !== windowInnerWidth) {
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-      setInnerWidth(newWidth);
-    }
+
+/*
+const [windowInnerWidth, setInnerWidth] = useState(0);
+function setRelativeVH() {
+  const newWidth = window.innerWidth;
+  if (newWidth !== windowInnerWidth) {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    setInnerWidth(newWidth);
   }
-  useEffect(() => {
-    setRelativeVH();
-    window.addEventListener('resize', setRelativeVH);
-    return () => window.removeEventListener('resize', setRelativeVH); 
-  });
-  // end height-100 fix
+}
+useEffect(() => {
+  setRelativeVH();
+  window.addEventListener('resize', setRelativeVH);
+  return () => window.removeEventListener('resize', setRelativeVH); 
+});
+// end height-100 fix
+*/
 
-  const [mainScreen, setMainScreen] = useState(HomepageScreen.INITIAL);
 
-  const handleLogoClick = e => {
-    e.preventDefault();
-    setMainScreen(HomepageScreen.INITIAL);
-  };
 
-  return (
-    <div>
-      <Head>
-        <title>schmiede.one - Home</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+export default class Homepage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mainScreen : HomepageScreen.INITIAL,
+    }
+    this.setMainScreen = this.setMainScreen.bind(this);
+  }
 
-      <article
-        className="Homepage"
-        style={{ backgroundImage: 'url("/homepage.jpg")' }}
-      >
-        <section className="container">
-          <div className="Homepage-NavStub">
-            <a
-              className="Homepage-LogoLink"
-              href="/"
-              onClick={handleLogoClick}
-            >
-              <img className="Homepage-Logo" src="/logo-white.png" alt="Logo" />
-            </a>
-          </div>
-        </section>
+  componentDidMount () {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
 
-        <main className="Homepage-Main container">
-          <Main screen={mainScreen} onScreenChange={setMainScreen} />
-        </main>
+  setMainScreen() {
+    this.setState({
+      mainScreen : HomepageScreen.INITIAL
+    })
+  }
+  render() {
+   
 
-        <section className="Homepage-MoreLinkWrapper container">
-          <Link href="/about">
-            <a className="Homepage-MoreLink">
-              <span>
-                <span style={{ whiteSpace: 'nowrap' }}>
-                  Don’t know yet? You can browse
+    const handleLogoClick = e => {
+      e.preventDefault();setMainScreen(HomepageScreen.INITIAL);};
+
+    return (
+
+      
+      <div>
+        <Head>
+          <title>Schmiede.one Innovation Lab</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+  
+        <article
+          className="Homepage"
+          style={{ backgroundImage: 'url("/homepage.jpg")' }}
+        >
+          <section className="container">
+            <div className="Homepage-NavStub">
+              <a
+                className="Homepage-LogoLink"
+                href="/"
+                onClick={handleLogoClick}
+              >
+                <img className="Homepage-Logo" src="/logo-white.png" alt="Logo" />
+              </a>
+            </div>
+          </section>
+  
+          <main className="Homepage-Main container">
+            <Main screen={this.state.mainScreen} onScreenChange={this.setMainScreen} />
+          </main>
+  
+          <section className="Homepage-MoreLinkWrapper container">
+            <Link href="/about">
+              <a className="Homepage-MoreLink">
+                <span>
+                  <span style={{ whiteSpace: 'nowrap' }}>
+                    Don’t know yet? You can browse
+                  </span>
+                  {' '}
+                  <span style={{ whiteSpace: 'nowrap' }}>
+                    around our website at any time.
+                  </span>
                 </span>
-                {' '}
-                <span style={{ whiteSpace: 'nowrap' }}>
-                  around our website at any time.
-                </span>
-              </span>
-              <img
-                className="Homepage-MoreLinkImage"
-                src="/arrow-right-button.svg"
-                alt="Visit website"
-              />
-            </a>
-          </Link>
-        </section>
-      </article>
-    </div>
-  );
+                <img
+                  className="Homepage-MoreLinkImage"
+                  src="/arrow-right-button.svg"
+                  alt="Visit website"
+                />
+              </a>
+            </Link>
+          </section>
+        </article>
+      </div>
+    );
+
+  }
+
+  
 }
